6f7cdaee9fb84dd7ba9beb4ff7fcb0367dc70afa
austin-m2 <morris.austinm@gmail.com>
2018-08-28 17:46:21 -0700

    Commit 2

--- src/CS_3700/hwOne.java
+++ src/CS_3700/hwOne.java
@@ -3,6 +3,6 @@ package CS_3700;
 public class hwOne {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, my name is Austin Morris");
     }
 }

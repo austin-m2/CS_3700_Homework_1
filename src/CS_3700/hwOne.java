45a51ba3c0761053db07c0cba27c642f596bcea4
austin-m2 <morris.austinm@gmail.com>
2018-08-28 17:47:27 -0700

    Commit 3

--- src/CS_3700/hwOne.java
+++ src/CS_3700/hwOne.java
@@ -3,6 +3,6 @@ package CS_3700;
 public class hwOne {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Austin Morris");
+        System.out.println("Goodbye, I finished my CS 3700 HW1");
     }
 }
